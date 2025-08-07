9b736729afcc7091629ec0722b9ac56ff38761b2
Pongsapat P. <tadzyz010@gmail.com>
2025-08-07 15:33:34 +0700

    Week1

--- hello.dart
+++ hello.dart
@@ -1,4 +1,40 @@
+import 'dart:io';
+
 void main() {
-  print("Hello Flutter!");
-  print("Hello FRAMYZ");
+  // String name = "Pongsapat";
+  // print("Hello $name");
+  // OUTPUT : Hello Pongsapat
+
+  // stdout >> import dart:io
+  // stdout.write('Hello-');
+  // stdout.write('Dart');
+  // OUTPUT : Hello-Dart
+
+  // GET USER INPUT
+
+  stdout.write('Enter your name : ');
+
+  // read user input as String?
+  // ? = nullable, ! = non-null
+  // String? = nullable string
+  // String name = stdin.readLineSync()!;
+  String? name = stdin.readLineSync();
+  if (name != null) {
+    print('Hello $name');
+  } else {
+    print('Name can not be empty!');
+  }
+
+  stdout.write('Enter your age : ');
+
+  String? age = stdin.readLineSync();
+  // Convert str to int
+  if (age != null) {
+    int? ageInt = int.tryParse(age);
+    if (ageInt != null) {
+      print('You are $ageInt years old.');
+    } else {
+      print('Please input only number!');
+    }
+  }
 }
