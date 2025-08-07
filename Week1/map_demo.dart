df047afd770295ddce98d49ec5f4f4d73a0e18c0
Pongsapat P. <tadzyz010@gmail.com>
2025-08-07 09:38:10 +0000

    Finish

--- Week1/map_demo.dart
+++ Week1/map_demo.dart
@@ -2,6 +2,12 @@ void main(){
   //Map is close to JS object, key:value
   Map<String, dynamic> student = {"name":"Peter","age":22,"gpax":3.65};
   print(student["name"]);
+<<<<<<< HEAD
+
+  //List of Map
+
+
+=======
 
   //List of Map
   List student = [
@@ -9,4 +15,5 @@ void main(){
       {"name":"Mary","age":19,"gpax":4.00}
   ];
 
+>>>>>>> 52226201b8ca8ae5e8bd099a9d83b74023ab5f23
 }
