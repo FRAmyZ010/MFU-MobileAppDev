f3da6d5569de0cbf11bcea0097b35a8b6f4edd04
Pongsapat P. <tadzyz010@gmail.com>
2025-08-07 15:55:26 +0700

    Total price exercise

--- Week1/list_demo.dart
+++ Week1/list_demo.dart
@@ -6,11 +6,30 @@ void main() {
   // List of int
   List<int> scores = [20, 15, 10];
 
-  print(users[0]); //Tom
-  print(users.length); //2
-  print(users.isEmpty); //false
+  // print(users[0]); //Tom
+  // print(users.length); //2
+  // print(users.isEmpty); //false
 
-  for (int i = 0; i < users.length; i++) {
-    print(users[i]);
+  // // for
+  // for (int i = 0; i < users.length; i++) {
+  //   print(users[i]);
+  // }
+  // // for .. in
+  // for (String user in users) {
+  //   print(user);
+  // }
+
+  // // forEach
+  // users.forEach((user) {
+  //   print(user);
+  // });
+
+  List<int> cart = [80, 55, 42];
+  // find total price
+  
+  int totalPrice = 0;
+  for (int i = 0; i < cart.length; i++) {
+    totalPrice += cart[i]; //totalPrice = totalPrice + cart[i]
   }
+  print("Total price : $totalPrice");
 }
