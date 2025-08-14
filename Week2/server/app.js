fc827b9926777b1e9942788972899b0985136c02
Pongsapat P. <tadzyz010@gmail.com>
2025-08-14 16:52:11 +0700

    d

--- Week2/server/app.js
+++ Week2/server/app.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
 
 const con = require('./db');
 
-app.get('/', (_req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// generate password
+app.get('/password/:raw',(req,res)=>{
+    const raw = req.params.raw;
+    bcrypt.hash(raw,10,(err,hash)=>{
+        if(err){
+            return res.status(500).send('Error creating password!');
+        }
+        res.send(hash);
+    });
+});
+
+// ----------------- login ------------------
+app.post('/login',(req,res)=>{
+    const {username, password} = req.body;
+    const sql = "SELECT * FROM users WHERE username = ?";
+    con.query(sql,[username],(err,result)=>{
+        if(err){
+            return res.status(500).send('Database error!');
+        }
+        if(result.length != 1){
+            return res.status(401).send('Invalid username or password!');
+        }
+        bcrypt.compare(password,result[0].password,(err,same)=>{
+            if(err){
+                return res.status(500).send('Error comparing passwords!');
+            }
+            if(same){
+                res.status(200).send('Login successful!');
+            }else{
+                res.status(401).send('Invalid username or password!');
+            }
+        })
+    })
+})
+
+
+// get all expenses
+app.get('/expenses', (_req, res) => {
     const sql = "SELECT * FROM expenses";
     con.query(sql,(err,result)=>{
         if(err){
