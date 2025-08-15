9f2814c821abc5f7e313fc06634c6782492ee89a
Pongsapat P. <tadzyz010@gmail.com>
2025-08-15 11:13:01 +0700

    a

--- Week2/server/app.js
+++ Week2/server/app.js
@@ -7,7 +7,7 @@ const con = require('./db');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// generate password
+//---------------------- generate password
 app.get('/password/:raw',(req,res)=>{
     const raw = req.params.raw;
     bcrypt.hash(raw,10,(err,hash)=>{
@@ -34,7 +34,10 @@ app.post('/login',(req,res)=>{
                 return res.status(500).send('Error comparing passwords!');
             }
             if(same){
-                res.status(200).send('Login successful!');
+                res.status(200).json({
+                    message:"Login successful!",
+                    user_id: result[0].id,
+                });
             }else{
                 res.status(401).send('Invalid username or password!');
             }
@@ -42,11 +45,40 @@ app.post('/login',(req,res)=>{
     })
 })
 
+// ----------------- register ------------------
+app.post('/register',(req,res)=>{
+    const {username, password} = req.body;
+        const sql = "SELECT * FROM users WHERE username = ?";
+        con.query(sql,[username],(err,result)=>{
+            if(err){
+                return res.status(500).send('Database error!');
+            }
+            if(result.length > 0){
+                return res.status(400).send('Username already exists!');
+            }
+            bcrypt.hash(password, 10,(err,hash)=>{
+                if(err){
+                    return res.status(500).send('Error creating password!');
+                }
+                const insert = "INSERT INTO users (username, password) VALUES (?, ?)";
+                con.query(insert,[username, hash],(err,result)=>{
+                    if(err){
+                        return res.status(500).send("Database error! Can't insert new account.");
+                    }
+                    res.status(200).send('User registered successfully!');
+                })
+            })
+
+        })
+    
+})
+
 
 // get all expenses
-app.get('/expenses', (_req, res) => {
-    const sql = "SELECT * FROM expenses";
-    con.query(sql,(err,result)=>{
+app.get('/expenses/:user_id', (req, res) => {
+    const userId = req.params.user_id;
+    const sql = "SELECT * FROM expenses WHERE user_id = ?";
+    con.query(sql,[userId],(err,result)=>{
         if(err){
             return res.status(500).send('Database error!');
         }
@@ -54,6 +86,23 @@ app.get('/expenses', (_req, res) => {
     })
 })
 
+
+// get all today's expense
+
+app.get('/expenses/today/:user_id', (req, res) => {
+    const userId = req.params.user_id;
+    
+    const sql = "SELECT * FROM expenses WHERE user_id = ? AND DATE(date) = CURDATE()";
+    con.query(sql,[userId],(err,result)=>{
+        if(err){
+            return res.status(500).json({error: 'Database error!'});
+        }
+        res.json(result);
+    })
+})
+
+
 app.listen(3000,()=>{
     console.log('Server 🤑 is running on port 3000 ✅');
 })
+
