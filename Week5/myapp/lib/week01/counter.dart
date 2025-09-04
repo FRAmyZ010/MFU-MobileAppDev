431f5cdcf543978f5b26705e71045dfa07ffa741
FRAmyZ <tadzyz010@gmail.com>
2025-09-04 16:01:28 +0700

    Refactor Counter widget to simplify state management and update UI display

--- Week5/myapp/lib/week01/counter.dart
+++ Week5/myapp/lib/week01/counter.dart
@@ -12,30 +12,38 @@ class Counter extends StatefulWidget {
 class _CounterState extends State<Counter> {
   // state variable
 
-  String msg = "Hello";
-  double click = 0;
+  int count = 0;
+
+  // methods
+  void update() {
+    setState(() {
+      count++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter app')),
-      body: Text(msg),
+      appBar: AppBar(
+        title: Text('Counter app', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text(
+          'Count = $count',
+          style: TextStyle(
+            fontSize: 20,
+            color: Color.fromARGB(255, 0, 100, 255),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // good for short code
-          click++;
-          // debugPrint('test');
-
-          if (click % 2 == 0) {
-            setState(() {
-              msg = 'Hello';
-            });
-          } else {
-            setState(() {
-              msg = 'Hi';
-            });
-          }
-        },
-        child: Text('Click'),
+        // onPressed: () {
+        //   update();
+        // },
+        onPressed: update,
+        backgroundColor: Colors.blue,
+        child: Text('Click', style: TextStyle(color: Colors.white)),
       ),
     );
   }
