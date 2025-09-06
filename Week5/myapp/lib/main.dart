44df6dcc7445917a2631093ed08061198b8ef094
FRAmyZ010 <tadzyz010@gmail.com>
2025-09-06 09:29:40 +0700

    🎉 Starting assignment 2

--- Week5/myapp/lib/main.dart
+++ Week5/myapp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week04/assignment/login_system.dart';
-
+import 'package:myapp/week04/assignment/calculator.dart';
 
 void main() {
-  runApp(MaterialApp(home: LoginSystem(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Calculator(), debugShowCheckedModeBanner: false));
 }
