e351f42475db0088b9e2a2a78cb836d6c7212d10
FRAmyZ010 <tadzyz010@gmail.com>
2025-09-06 09:56:22 +0700

    Setting UI

--- Week5/myapp/lib/week04/assignment/calculator.dart
+++ Week5/myapp/lib/week04/assignment/calculator.dart
@@ -8,8 +8,79 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  TextEditingController tcNum1 = TextEditingController();
+  TextEditingController tcNum2 = TextEditingController();
+
+  String ans = "";
+
+  // Sum function
+
+  void Sum() {}
+
+  void Power() {}
+  void Clear() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Calculator')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(),
+            child: TextField(
+              controller: tcNum1,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Number 1",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(),
+            child: TextField(
+              controller: tcNum1,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Number 2",
+              ),
+            ),
+          ),
+
+          ElevatedButton(
+            onPressed: Sum,
+            child: Text('Sum'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              foregroundColor: Colors.black,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(ans),
+
+          ElevatedButton(
+            onPressed: Power,
+            child: Text('Sum'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 7, 147, 255),
+              foregroundColor: Colors.white,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(ans),
+
+          ElevatedButton(
+            onPressed: Clear,
+            child: Text('Sum'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 254, 0, 0),
+              foregroundColor: Colors.white,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(ans),
+        ],
+      ),
+    );
   }
 }
