a317b7c8a39e8168ad77ce2f2cb1858fa730c5df
FRAmyZ010 <tadzyz010@gmail.com>
2025-09-20 08:12:18 +0700

    Complete week6 asm #2

--- Week5/myapp/lib/main.dart
+++ Week5/myapp/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/asm/asm1.dart';
+import 'package:myapp/week06/asm/asm2.dart';
 
 void main() {
-  runApp(MaterialApp(home: Asm1(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Asm2(), debugShowCheckedModeBanner: false));
 }
